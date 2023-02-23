b9b0e26309b3cdccb66eca764994b8e8e6c1fe85
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-23 12:53:01 -0600

    got buttons to click

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ $(document).ready(onReady);
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let newHealth = 50;
+let attackPoint = 100;
 
 function onReady() {
+    console.log('in onReady function')
+    $('.attack-btn.arcane-sceptre').on('click', decreaseHealth)
     
+    $('.attack-btn entangle').on('click', decreaseHealth)
+
+    $('.attack-btn dragon-blade').on('click', decreaseHealth)
+
+    $('.attack-btn star-fire').on('click', decreaseHealth)
+
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
@@ -15,4 +26,14 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+    render()
+}
+
+function decreaseHealth(){
+    console.log('in decreaseHealth function');
+    newHealth --;
+}
+
+function render(){
+    $(".hp-text").html(newHealth)
 }
